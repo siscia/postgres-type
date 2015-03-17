48a25973df0882226993544e0a094d50af75f0dd
siscia <simone@mweb.biz>
2015-03-17 04:34:56 +0100

    new version update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject postgre-types "0.0.2"
+(defproject postgre-types "0.0.3"
   :description "Integration with PostgresSQL multiple types"
   :url "https://github.com/siscia/postgres-type"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.3"]
-                 [org.postgresql/postgresql "9.3-1102-jdbc41"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :profiles {:dev {:dependencies [[cheshire "5.3.1"]]}})
