c74c66e40d22c1e48b8121143b70bfd8d8011ca4
Eric Brown <brown@fastmail.com>
2018-07-04 08:05:03 -0500

    Change name of project to postgres-json

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject postgre-types "0.0.4"
+(defproject postgres-json "0.0.4"
   :description "Integration with PostgresSQL multiple types"
-  :url "https://github.com/siscia/postgres-type"
+  :url "https://github.com/ecbrown/postgres-json"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]]
-  :repositories [["release" "https://clojars.org/siscia"]]
+  :repositories [#_["release" "https://clojars.org/siscia"]]
   :profiles {:dev {:dependencies [[cheshire "5.3.1"]]}})
